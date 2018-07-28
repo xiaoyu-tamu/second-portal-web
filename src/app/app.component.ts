c3c94e345c0c5d583e41d61043d3d61a0cfb92fc
michael <lxyamerica@gmail.com>
2018-07-27 22:18:22 -0500

    chore: change css preprocessor to stylus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const api = 'http://localhost:3000/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.styl'],
 })
 export class AppComponent {
+  constructor(http: HttpClient) {
+    http.get<{ app_name: string }>(api).subscribe((res) => {
+      console.log(res);
+      this.title = res.app_name;
+    });
+  }
   title = 'app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { AwesomeComponent } from './awesome/awesome.component';
+import { SiderComponent } from './layout/sider/sider.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, HeaderComponent, AwesomeComponent, SiderComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
