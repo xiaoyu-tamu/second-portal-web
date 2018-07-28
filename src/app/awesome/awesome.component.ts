801574645f6b31fd05323ced9c6e831bd12b8948
michael <lxyamerica@gmail.com>
2018-07-27 22:50:48 -0500

    chore: setup boilerplates

---
+++ src/app/awesome/awesome.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-awesome',
+  templateUrl: './awesome.component.html',
+  styleUrls: ['./awesome.component.styl'],
+})
+export class AwesomeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ src/app/layout/sider/sider.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sider',
+  templateUrl: './sider.component.html',
+  styleUrls: ['./sider.component.styl'],
+})
+export class SiderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
